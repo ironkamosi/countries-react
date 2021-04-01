d35fa0ca56d74f78fefe3050cfa81b6b84337a7b
ironkamosi <alintonchambers@gmail.com>
2021-04-01 12:29:50 +0200

    completed level 2

--- countries-react/src/App.js
+++ countries-react/src/App.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React,{useState} from "react"
 import './App.css';
 import CountriesDisplayCards from "./CountriesDisplayCards.js"
 import "bootstrap/dist/css/bootstrap.min.css";
+import SearchBar from "./SearchBar.jsx"
+import countriesAll from "./countriesAll.json"
 
 function App() {
-  return <CountriesDisplayCards />;
+const [foundCountryName, setCountryName] = useState(countriesAll)
+  return (
+    <div>
+      <SearchBar setCountryName={setCountryName} />
+      <CountriesDisplayCards filteredCountriesData={foundCountryName}/>
+    </div>
+  )
 }
 
 export default App;
+// setting countriesDisplay card with the filtered from the search ie foundCountryName
--- countries-react/src/CountriesDisplayCards.js
+++ countries-react/src/CountriesDisplayCards.js
@@ -1,11 +1,12 @@
 import React from "react";
-import countriesAll from "./countriesAll.json";
+// import countriesAll from "./countriesAll.json";
 import ContainerCard from "./ContainerCard";
 import "./CountriesDisplayCards.css"
-const CountriesDisplayCards = () => {
+
+const CountriesDisplayCards = (props) => {
   return (
     <div className="countryDCardsContainer">
-      {countriesAll.map((element, index) => {
+      {props.filteredCountriesData.map((element, index) => {
         return <ContainerCard key={index} country={element} />;
       })}
     </div>
